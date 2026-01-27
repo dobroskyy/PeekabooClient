9eb0da0b7bedc65b88d428206f24c68dfaa5e34e
dobroskyy <dobrozhinsky@gmail.com>
2026-01-27 18:20:58 +0300

    Initial Commit

---
+++ PeekabooClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeekabooClient
+//
+//  Created by Максим on 27.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
